468d23a4203acad912e5627de5fcac286ecc3e12
Chris  Oliver <coliver303@gmail.com>
2014-08-08 13:58:02 -0700

    minimum panel sliding functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,24 @@
 // Cached Selectors
 
 var $filterBox = $('.filterBox');
+var $arrow = $('.arrow');
+var $openNode = $('.openNode');
 
-$filterBox .find('ul').hide()
+// on load
+$filterBox.find('ul').toggle();
+$openNode.hide();
 
+// filterbox slidedown
 $filterBox.on('click', function(){
-	$(this).find('ul').slideToggle();
+	$(this).find('ul').slideToggle({
+		duration: 'fast',
+		easing: 'linear'
+	});
+});
+
+
+// node slie down
+$arrow.on('click', function(){
+	console.log("hey");
+	$(this).parent().next().slideToggle();
 });

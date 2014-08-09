333e65cc0dfcb1a98b545d1695d7675ff0619bcf
Chris  Oliver <coliver303@gmail.com>
2014-08-08 21:00:29 -0700

    sticky root and predicate node hovers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,13 @@
 var $filterBox = $('.filterBox');
 var $arrow = $('.arrow');
 var $openNode = $('.openNode');
+var $predicateNode = $('.predicateNode');
+var $rootFromHere = $('.rootFromHere > a');
 
 // on load
 $filterBox.find('ul').toggle();
 $openNode.hide();
+$rootFromHere.hide();
 
 // filterbox slidedown
 $filterBox.on('click', function(){
@@ -22,4 +25,14 @@ $filterBox.on('click', function(){
 // node slie down
 $arrow.on('click', function(){
 	$(this).parent().next().slideToggle();
+});
+
+// sticky
+
+$("#rootNode").sticky({topSpacing:20});
+
+// predicate Nodes
+
+$predicateNode.on("mouseenter", function(){
+	$rootFromHere.show();
 });

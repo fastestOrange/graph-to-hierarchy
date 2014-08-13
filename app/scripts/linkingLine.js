f8d176728d99c85fd73715e6d8442bacf156e614
Chris  Oliver <coliver303@gmail.com>
2014-08-12 22:00:39 -0700

    links add and remove todo: multiple lines

--- app/scripts/linkingLine.js
+++ app/scripts/linkingLine.js
@@ -1,47 +1,31 @@
 $('.nodeWrapper').click(function(){
-var linkLine = $('<div></div>').appendTo('body');
-    
-linkLine
-    .addClass('linkLine')
-    .css('top', $('#rootNode').offset().top + $('#rootNode').outerWidth() / 2)
-    .css('left', $('#rootNode').offset().left + $('#rootNode').outerHeight() / 2);
-
-nodeLink($(this));
-
-    
+    var toggle = $(this).data("selected");
+    var linkLine = $('<div></div>').appendTo('body');
+    linkLine
+        .addClass('linkLine')
+        .css('top', $('#rootNode').offset().top + $('#rootNode').outerWidth() / 2)
+        .css('left', $('#rootNode').offset().left + $('#rootNode').outerHeight());
+    if(!toggle){
+        rootNodeLink($(this)); 
+        $(this).data("selected", true);  
+        
+    }else{
+        $('.linkLine').remove();
+        $(this).data("selected", false);
+        
+    }   
 });
 
-    
-    // $(document).mousemove(linkMouseMoveEvent);
+function rootNodeLink(target) {
 
-    // Cancel on right click
-    // $(document).bind('mousedown.link', function(event) {
-    //     if(event.which == 3) {
-    //         endLinkMode();
-    //     }
-    // });
-
-    // $(document).bind('keydown.link', function(event) {
-    //     // ESCAPE key pressed
-    //     if(event.keyCode == 27) {
-    //         endLinkMode();
-    //     }
-    // });
-// });
-    
-function nodeLink(target) {
+        
 
         var originX = $('#rootNode').offset().left + $('#rootNode').outerWidth() / 2;
         var originY = $('#rootNode').offset().top + $('#rootNode').outerHeight() / 2; 
 
-        console.log(target[0].offsetTop); 
-
-        var destX = target.offset().left;
-
+        var destX = target.offset().left - 80;
         var destY = target.offset().top + target.outerHeight() / 2;
 
-        // console.log("Ox" + originX + ", Oy" + originY); 
-        
         var length = Math.sqrt((destX - originX) * (destX - originX) 
             + (destY - originY) * (destY - originY));
     
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,26 +2,6 @@
 
 //line
 
-function createLine(x1,y1, x2,y2){
-    var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-  var angle  = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-  var transform = 'rotate('+angle+'deg)';
-
-    var line = $('<div>')
-        .appendTo('#container')
-        .addClass('line')
-        .css({
-          'position': 'absolute',
-          'transform': transform
-        })
-        .width(length)
-        .offset({left: x1, top: y1});
-
-    return line;
-}
-
-createLine(25,25,100,100);
-
 // flags
 
 var constituentsOpen = false;
@@ -37,7 +17,6 @@ var $nodeWrapper = $('.nodeWrapper');
 
 
 // on load
-// $filterBox.find('ul').toggle();
 $('.column:eq(1)').children().hide();
 $openNode.hide();
 
@@ -50,10 +29,6 @@ $nodeWrapper.on('click', function(){
 });
 
 $('div[data-col2Index="3"]').click(function(){
-		var line3=new Line(8,76,130,168);
-		secondContext.clearRect(0, 0, canvas.width, canvas.height);
-            // draw the line
-    line3.drawWithArrowheads(secondContext);
 	
 });
 
@@ -112,18 +87,7 @@ var menuTipClick = function(obj){
 		obj.parent().data('selected', false);
 	}
 
-// RAPHAEL
 
-// var paper = Raphael(0,90, 500, 500);
-
-// var a = paper.path("M 183  293 L 326  230");
-// var b = paper.path("M183 293L326 300");
-// var c = paper.path("M183 293L326 378");
-
-
- 
-
-// drawToSecondNodes(8, 76, 7);
 
 
 
